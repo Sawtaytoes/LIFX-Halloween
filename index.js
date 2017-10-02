f246c6b526e34a91faa159ef2b4bd2c60e507e44
Kevin Ghadyani <Saturn2888@gmail.com>
2017-10-02 00:29:31 -0500

    Randomized cycles and period in LIFX breathe command

--- index.js
+++ index.js
@@ -16,18 +16,20 @@ const headers = {
 
 const spookyLightInterval = 10000
 
+const getCycles = () => Math.ceil(Math.random() * 3)
+const getPeriod = () => Math.random()
 
 const doScaryLightFlash = () => (
-	// Promise.resolve({ json: () => Promise.resolve(lightId) })
 	fetch(
 		`${lifxApi}v1/lights/${config.getLifxSelector()}:random/effects/breathe`,
 		{
 			body: (
 				JSON.stringify({
+					// hue:120 saturation:1.0 brightness:0.5
 					color: 'purple',
-					cycles: 2,
+					cycles: getCycles(),
 					from_color: 'orange',
-					period: 0.5,
+					period: getPeriod(),
 					// peak: 0,
 					// power_on: false,
 				})
