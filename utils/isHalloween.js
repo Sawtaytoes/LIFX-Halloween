f2b8c55291d94b52b6a7539fff1b6ee24a851eae
Kevin Ghadyani <Saturn2888@gmail.com>
2018-10-15 01:27:16 -0500

    Use direct composition for isHalloween so it keeps the same API as before
    isHalloween is no longer as semantic enough. Eventually, it should be replaced outright.

--- utils/isHalloween.js
+++ utils/isHalloween.js
@@ -1,11 +1,9 @@
 const isDuringHalloweenNight = require('./isDuringHalloweenNight')
 
-const isHalloween = (
-	date,
-) => (
-	date instanceof Date
-	? isDuringHalloweenNight(date)
-	: isDuringHalloweenNight(new Date())
+const isHalloween = () => (
+	isDuringHalloweenNight(
+		new Date()
+	)
 )
 
 module.exports = isHalloween
