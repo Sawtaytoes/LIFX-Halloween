c9bfa48aa9ca32d4953f68ee9ca780df44269423
Kevin Ghadyani <Saturn2888@gmail.com>
2019-10-09 20:27:18 -0500

    Updated server.sh

--- server.sh
+++ server.sh
@@ -1,10 +1,4 @@
 #/bin/bash
 cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-numberOfServers=$1
-
-if [[ ! $numberOfServers ]]; then
-	numberOfServers=0
-fi
-
-pm2 start npm -i $numberOfServers --name ${PWD##*/} -- run server
+pm2 start npm -i 1 --name ${PWD##*/} -- run server
